6413b3221fc64daee4edde4a6af1d749aee2e35a
Sandeep Kumar <sandeep.22.kumar@bt.com>
2018-06-28 01:02:15 +0530

    error logging in deploy.js

--- deploy.js
+++ deploy.js
@@ -20,6 +20,11 @@ function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body);
   }
+  else {
+	  console.log(error);
+	  console.log(response);
+  }
 }
 
+console.log('requesting: ' + option.url)
 request(options, callback);
